a54d1291dfd4bcbeef2f818388871eacddf3d4a8
IkyIsaac <isaackitomari33@gmail.com>
2024-12-16 22:28:46 +0300

    Add Infoboxes

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
 import Link from 'next/link';
 import React from 'react'
 
@@ -8,10 +10,10 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div> 
-      <h1 className='text-3xl'>Welcome</h1>
-      <Link href='/properties'>Show Properties</Link>
-    </div>
+    <>
+    <Hero/>
+    <InfoBoxes/>
+    </>
   )
 }
 
